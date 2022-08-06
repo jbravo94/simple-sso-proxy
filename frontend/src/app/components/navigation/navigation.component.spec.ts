19b1b24b75be2f5f4bb81af3c4f19b3f225d731f
Johannes HEINZL <jh.heinzl@gmail.com>
2022-08-06 23:39:01 +0200

    Adds first frontend tests

--- frontend/src/app/components/navigation/navigation.component.spec.ts
+++ frontend/src/app/components/navigation/navigation.component.spec.ts
@@ -8,13 +8,19 @@ import { NavigationComponent } from './navigation.component';
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
   let fixture: ComponentFixture<NavigationComponent>;
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
+    const localAuthServiceSpy = jasmine.createSpyObj('AuthService', ['getLoggedIn', 'isAdmin']);
+
     await TestBed.configureTestingModule({
-      imports: [ RouterTestingModule, HttpClientTestingModule ],
-      declarations: [ NavigationComponent ],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [NavigationComponent],
+      providers: [{ provide: AuthService, useValue: localAuthServiceSpy }]
     })
-    .compileComponents();
+      .compileComponents();
+
+    authServiceSpy = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
 
     fixture = TestBed.createComponent(NavigationComponent);
     component = fixture.componentInstance;
@@ -23,5 +29,50 @@ describe('NavigationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('is logged out view valid', () => {
+
+    authServiceSpy.getLoggedIn.and.returnValue(false);
+    authServiceSpy.isAdmin.and.returnValue(false);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(component.isLoggedIn()).toBeFalsy();
+    expect(component.isAdmin()).toBeFalsy();
+
+    expect(compiled.querySelector('#sidebar-menu-button')).toBeFalsy();
+    expect(compiled.querySelector('#sidebar-settings-button')).toBeFalsy();
+    expect(compiled.querySelector('#sidebar-logout-button')).toBeFalsy();
+  });
+
+  it('is logged in view valid', () => {
+
+    authServiceSpy.getLoggedIn.and.returnValue(true);
+    authServiceSpy.isAdmin.and.returnValue(false);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(component.isLoggedIn()).toBeTruthy();
+    expect(component.isAdmin()).toBeFalsy();
+
+    expect(compiled.querySelector('#sidebar-menu-button')).toBeTruthy();
+    expect(compiled.querySelector('#sidebar-settings-button')).toBeFalsy();
+    expect(compiled.querySelector('#sidebar-logout-button')).toBeTruthy();
+  });
+
+  it('is admin view valid', () => {
+
+    authServiceSpy.getLoggedIn.and.returnValue(true);
+    authServiceSpy.isAdmin.and.returnValue(true);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(component.isLoggedIn()).toBeTruthy();
+    expect(component.isAdmin()).toBeTruthy();
+
+    expect(compiled.querySelector('#sidebar-menu-button')).toBeTruthy();
+    expect(compiled.querySelector('#sidebar-settings-button')).toBeTruthy();
+    expect(compiled.querySelector('#sidebar-logout-button')).toBeTruthy();
   });
 });
